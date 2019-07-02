d62c647d1ef7329284d6a17c2f5adba3f8baeb40
tvibhu12 <tvibhu12@gmail.com>
2019-07-02 20:12:13 +0530

    add our API and handle response data

--- src/container/App.js
+++ src/container/App.js
@@ -1,33 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Article from '../components/Article';
+import { Dropdown } from 'semantic-ui-react'
 
-class App extends React.Component {
-  componentDidMount(){
-    fetch('https://newsapi.org/v2/sources?apiKey=dec65431b9ca47ee84e912af51a569fa').then((response)=>response.json()).then(rea=>console.log(rea));
+const friendOptions = [
+  {
+    text: 'Reuters',
+    value: 'reuters',
+    image: { avatar: true, src: 'http://s3.reutersmedia.net/resources_v2/images/favicon/favicon-96x96.png' },
+  },
+  {
+    text: 'Daily Mail',
+    value: 'daily-mail',
+    image: { avatar: true, src: 'http://www.dailymail.co.uk/apple-touch-icon.png' },
+  },
+  {
+    text: 'Hacker News',
+    value: 'hacker-news',
+    image: { avatar: true, src: 'https://news.ycombinator.com/favicon.ico' },
+  },
+  {
+    text: 'The Verge',
+    value: 'the-verge',
+    image: { avatar: true, src: 'http://cdn2.vox-cdn.com/uploads/chorus_asset/file/7395359/ios-icon.0.png' },
+  },
+  {
+    text: 'The Next Web',
+    value: 'the-next-web',
+    image: { avatar: true, src: 'https://cdn2.tnwcdn.com/wp-content/themes/cyberdelia/assets/icons/apple-touch-icon-120x120.png?v=1492167659' },
+  },
+  {
+    text: 'The Telegraph',
+    value: 'the-telegraph',
+    image: { avatar: true, src: 'https://icons.better-idea.org/lettericons/T-120-1a151b.png' },
+  },
+  {
+    text: 'TechCrunch',
+    value: 'techcrunch',
+    image: { avatar: true, src: 'https://s0.wp.com/wp-content/themes/vip/techcrunch-2013/assets/images/homescreen_TCIcon_ipad_2x.png' },
+  },
+  {
+    text: 'Reddit /r/all',
+    value: 'reddit-r-all',
+    image: { avatar: true, src: 'https://www.reddit.com/apple-touch-icon-precomposed.png' },
+  },
+  {
+    text: 'Buzzfeed',
+    value: 'buzzfeed',
+    image: { avatar: true, src: 'https://www.buzzfeed.com/static-assets/img/touch-icon-ios_120.208a0e329cd6e8d831b21ae17fb6aabb.png' },
+  },
+];
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      token: 'dec65431b9ca47ee84e912af51a569fa',
+      sortBy: 'latest',
+      source: 'buzzfeed',
+      articles: [],
+    }
+
+    this.getArticles = this.getArticles.bind(this);
   }
-  render(){
-   
+
+  handleChange = (e, { value }) => {
+    this.setState({ articles: [] });
+    this.setState({ source: value }, this.getArticles);
+  };
+
+  componentDidMount() {
+    this.getArticles()
+  }
+
+  getArticles(){
+    fetch(`https://newsapi.org/v1/articles?source=${this.state.source}&sortBy=${this.state.sortBy}&apiKey=${this.state.token}`)
+    .then( (response) => response.json())
+    .then( (json) => this.setState({articles: json.articles}));
+  }
+
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div className="container">
+        <div className="top">
+          Show me posts by
+          {' '}
+          <Dropdown
+            inline
+            defaultValue={this.state.source}
+            onChange={this.handleChange}
+            options={friendOptions}
+            defaultValue={friendOptions[0].value}
+          />
+        </div>
+        <div className="articles">
+          {
+            this.state.articles.length ? this.state.articles.map((item, index)=>{
+              return <Article
+                key={index}
+                title={item.title}
+                text={item.description}
+                url={item.url}
+                image={item.urlToImage ? item.urlToImage : 'http://placehold.it/250'}
+              />
+            }) : <Article />
+          }
+        </div>
       </div>
+
     );
   }
-  
-}
 
-export default App;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './containers/App'; 
+import App from './container/App'; 
 import 'semantic-ui/dist/semantic.min.css';
 import * as serviceWorker from './serviceWorker';
 
